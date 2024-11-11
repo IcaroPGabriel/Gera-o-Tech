47a0a436292be07f4b1914d69e35c6b93bda93e4
IcaroPGabriel <icaropereiragabriel@outlook.com>
2024-11-11 12:00:46 -0300

    Atualizando novos endpoints
    Atualizando novos endpoints

--- Back-End/Node1/src/controllers/UsersController.js
+++ Back-End/Node1/src/controllers/UsersController.js
@@ -24,32 +24,90 @@ const UsersRegister = async (req,res,next) => {
         const bcrypt = require('bcrypt')
         const saltRound = 10
         const hash = await bcrypt.hash(password, saltRound)
+
+        const EmailValid = await UserModel.findOne({ 
+            where: { email: email } });
     
     try {
-            const userCreate = await UserModel.create({
-                firstname:firstname,
-                surname:surname,
-                email:email,
-                password:hash
-            });
+        const userCreate = await UserModel.create({
+            firstname:firstname,
+            surname:surname,
+            email:email,
+            password:hash
+        });
 
+        if (EmailValid == true) {
+            res.send({
+                sucess: true,
+                message: `Email já cadastrado!`
+        })
+        } else {
         res.status(201).send({
             sucess: true,
             message: `Usuário criado com sucesso ${userCreate.firstname - userCreate.id}`
-        })
+        }
+        } catch (error) {
+            res.status(400).send({
+                sucess:false,
+                message: `Falha no cadastro!!!${error}`
+            })
+        }
+    }
+const UserUpdate = async (req,res, next) => {
+    try {
+        const id = req.params.id
+        const user = await UserModel.update(req.body, {
+            where: { id }
+        });
+
+        if (user == true) {
+            res.status(200).send({
+                'sucess': true,
+                'message': `Usuário alterado com sucesso! ID: ${user.id - user.name}`
+            })
+            
+        } else {
+            res.send({
+                'sucess': true,
+                'message': `Usuário não encontrado!`
+            })
+
+            
+        }
     } catch (error) {
-        res.status(400).send({
-            sucess:false,
-            message: `Falha no cadastro!!!${error}`
+        res.send({
+            'sucess': false,
+            'message': `Falha na listagem de usuários${error}`
         })
     }
+}
 
+const UserDelete = async (req,res,next) => {
+   
+    try {
+        const id = req.params.id
+        const user = await UserModel.destroy( {
+            where: { id }
+        });
+       
+        res.status(200).send({
+            'sucess': true,
+            'message': `Usuário deletado com sucesso! ID: ${user.id - user.name}`
+        })
 
+        
+    } catch (error) {
+        res.send({
+            'sucess': false,
+            'message': `Falha na listagem de usuários${error}`
+        })
+    }
 }
 
 
+
 module.exports = {
-    UsersList, UsersRegister
+    UsersList, UsersRegister, UserUpdate, UserDelete
 }
 
 
--- Back-End/Node1/src/routes/RouterUsers.js
+++ Back-End/Node1/src/routes/RouterUsers.js
@@ -4,5 +4,7 @@ const UsersController = require('../controllers/UsersController')
 
 router.get('/', UsersController.UsersList)
 router.post('/', UsersController.UsersRegister)
+router.put('/:id', UsersController.UserUpdate)
+router.delete('/:id', UsersController.UserDelete)
 
 module.exports = router
